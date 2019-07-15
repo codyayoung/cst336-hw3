fca20ed98dea91351a7fd36b2e4386a995b1ffe6
Cody Young <cyoung1623@gmail.com>
2019-07-14 17:12:48 -0700

    Changed API to exchangerates.io

--- js/currency_convert.js
+++ js/currency_convert.js
@@ -1,12 +1,10 @@
-var endpoint = 'latest';
-var access_key = '4bcc15953df748344dc29b93d16439fc';
-var base = 'USD';
+var base = "USD";
 
 // On window load, populates currency select menus 
 $(document).ready(function(){
     $.ajax({
         method: "GET",
-            url: 'http://data.fixer.io/api/' + endpoint + '?access_key=' + access_key,
+            url: "https://api.exchangeratesapi.io/latest?" + "base=" + base,
         dataType: "json",
             data: {"rates": $('#rates').val() },
     
